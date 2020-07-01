33d89ba98b5147ad2989a0b4e584ed4f5685ab59
Jacques Gignoux <jacques.gignoux@upmc.fr>
2020-07-01 16:52:02 +0200

    code + version cleanup

--- rvgrid/scripts/fr/cnrs/iees/versioning/LicenseManager.java
+++ rvgrid/scripts/fr/cnrs/iees/versioning/LicenseManager.java
@@ -1,3 +1,32 @@
+/**************************************************************************
+ *  RVGRID - A light-weight rendezvous system                             *
+ *                                                                        *
+ *  Copyright 2018: Shayne Flint, Jacques Gignoux & Ian D. Davies         *
+ *       shayne.flint@anu.edu.au                                          * 
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  RVGRID is a A light-weight implementation of ADA's rendez-vous        *
+ *  messaging pattern                                                     *
+ *                                                                        *   
+ **************************************************************************
+ *  This file is part of RVGRID.                                          *
+ *                                                                        *
+ *  RVGRID is free software: you can redistribute it and/or modify        *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  RVGRID is distributed in the hope that it will be useful,             *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with RVGRID.                                                    *
+ *  If not, see <https://www.gnu.org/licenses/gpl.html>                   *
+ *                                                                        *
+ **************************************************************************/
 package fr.cnrs.iees.versioning;
 
 import java.io.File;
