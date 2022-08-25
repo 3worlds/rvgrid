44bec3c54f698606c3d4d80fe8f724776d3f4438
Jacques Gignoux <jacques.gignoux@upmc.fr>
2022-08-25 10:51:28 +0200

    fixed javadoc location in VersionManager

--- rvgrid/scripts/fr/cnrs/iees/versioning/VersionManager.java
+++ rvgrid/scripts/fr/cnrs/iees/versioning/VersionManager.java
@@ -1,32 +1,3 @@
-/**************************************************************************
- *  RVGRID - A light-weight rendezvous system                             *
- *                                                                        *
- *  Copyright 2018: Shayne Flint, Jacques Gignoux & Ian D. Davies         *
- *       shayne.flint@anu.edu.au                                          * 
- *       jacques.gignoux@upmc.fr                                          *
- *       ian.davies@anu.edu.au                                            * 
- *                                                                        *
- *  RVGRID is a A light-weight implementation of ADA's rendez-vous        *
- *  messaging pattern                                                     *
- *                                                                        *   
- **************************************************************************
- *  This file is part of RVGRID.                                          *
- *                                                                        *
- *  RVGRID is free software: you can redistribute it and/or modify        *
- *  it under the terms of the GNU General Public License as published by  *
- *  the Free Software Foundation, either version 3 of the License, or     *
- *  (at your option) any later version.                                   *
- *                                                                        *
- *  RVGRID is distributed in the hope that it will be useful,             *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *  GNU General Public License for more details.                          *                         
- *                                                                        *
- *  You should have received a copy of the GNU General Public License     *
- *  along with RVGRID.                                                    *
- *  If not, see <https://www.gnu.org/licenses/gpl.html>                   *
- *                                                                        *
- **************************************************************************/
 package fr.cnrs.iees.versioning;
 
 /**********************************************************************************************
@@ -126,7 +97,7 @@ public class VersionManager {
 
 	"\t<!-- Artifacts are produced in the project lib directory-->\n" +
 	"\t<property name=\"jarRepo\" location=\"${user.dir}/../lib\"/>\n\n" +
-	"\t<property name=\"docRepo\" location=\"${user.dir}/../doc/javadoc\"/>" +
+	"\t<property name=\"docRepo\" location=\"${user.dir}/../javadoc\"/>" +
 
 	"\t<!-- Use this string as the organisation package -->\n" +
 	"\t<property name=\"org\" value=\"" + ORG +"\"/>\n\n" +
