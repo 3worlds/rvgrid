8485952561ea087f5163f6d44792b44837fcf086
Ian Davies <ian.davies@anu.edu.au>
2022-10-20 11:50:47 +1100

    refactoring

--- rvgrid/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ rvgrid/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -83,7 +83,7 @@ public class VersionSettings {
 	 * 
 	 */
 	protected static String[][] DEPS = { 
-			{"fr.ens.biologie", "generics", "[0.4.0,)", null},
+			{"fr.cnrs.iees.omhtk", "generics", "[0.4.0,)", null},
 	};
 	
 	/** The name of the main class to put in the jar manifest, if any. This enables users to
